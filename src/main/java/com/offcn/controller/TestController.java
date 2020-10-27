71b39ddf406c3206d59b2b81e724b2f112600305
wuhai <1339299907@qq.com>
2020-10-27 21:56:09 +0800

    新建一个循环

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.offcn.controller;
 
 public class TestController {
-    //add by wh
-    System.out.println("test");
+
 
     public String test(){
+        //add by wh
+        System.out.println("test");
+        //add by wh
+        while (true){
+
+        }
         return null;
     }
 }
